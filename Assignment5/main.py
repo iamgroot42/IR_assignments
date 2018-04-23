0e19af51ecb086fe4472077a468fd78a480b6f83
Anshuman Suri <anshuman14021@iiitd.ac.in>
2018-04-23 16:07:29 +0530

    Submitting

--- Assignment5/main.py
+++ Assignment5/main.py
@@ -39,7 +39,7 @@ def load_data(dirFolder, featureKeepRatio=1.0):
     return np.stack(X)
 
 
-def load_data_word2vec(dirFolder):
+def load_data_word2vec(dirFolder, featureKeepRatio=1.0):
     model = gensim.models.keyedvectors.KeyedVectors.load_word2vec_format('./GoogleNews-vectors-negative300.bin', binary=True)
     classes = sorted(os.listdir(dirFolder))
     vocabulary = set()
@@ -56,11 +56,17 @@ def load_data_word2vec(dirFolder):
     # Featurize data according to above vocabulary
     vocabulary = list(vocabulary)
     X = []
+    def getIt(dablu):
+        try:
+            return model[dablu]
+        except:
+            return np.zeros((300,))
+
     for i, dclass in enumerate(classes):
         for doc in allDocs[i]:
             processedFile = preprocess.readFile(os.path.join(os.path.join(dirFolder, dclass), doc))
             words = list(set(processedFile))
-            features = [ model[w] for w in vocabulary]
+            features = [ getIt(w) for w in vocabulary]
             X.append(features)
     return np.stack(X)
 
@@ -74,11 +80,9 @@ def KM(X, K):
 if __name__ == "__main__":
 	args = parser.parse_args()
 	# BOW features:
-	#X_train = load_data(args.datapath, args.keepratio)
-	#X_train = np.stack(X_train) #[x/np.linalg.norm(x) for x in X_train])
+	X_train = load_data(args.datapath, args.keepratio)
 	# Word2Vec features
-	load_data_word2vec(args.datapath)
-	exit()
+	#X_train = load_data_word2vec(args.datapath, args.keepratio)
 	print(X_train.shape[1], "vocab size")
 	print(X_train.shape[0], "number of points")
 	loss = KM(X_train, args.K)
